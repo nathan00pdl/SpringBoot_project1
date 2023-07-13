41541ea64f81a5610106d1bb8687e815a1198da8
Nathan Paiva <nathanpaiva00@gmail.com>
2023-07-13 13:41:48 -0300

    Homolog check

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -6,7 +6,7 @@ public interface GameMinProjection {
 	//Declarando métodos getters para cada dado que a consulta irá retornar
 	Long getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
